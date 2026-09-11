921f0b368f3d839c26178e87ef367366c6f2c3af
Bhaskar Bhatt <8h45k4r@gmail.com>
2026-09-11 09:17:44 +0545

    Fix Blade directive tokenisation in attribution component

--- resources/views/components/site/attribution.blade.php
+++ resources/views/components/site/attribution.blade.php
@@ -1,5 +1,9 @@
 @props(['name', 'url', 'license', 'licenseUrl', 'citation' => null, 'note' => null, 'date' => null])
 <aside {{ $attributes->merge(['class' => 'rule pt-3 text-xs text-brand-muted leading-5']) }} aria-label="Data attribution">
-    <p><span class="font-medium text-brand-navy">Source:</span> <a href="{{ $url }}" rel="noopener">{{ $name }}</a>, licensed <a href="{{ $licenseUrl }}" rel="license noopener">{{ $license }}</a>.@if($date) Snapshot {{ $date }}.@endif@if($note) {{ $note }}@endif</p>
+    <p>
+        <span class="font-medium text-brand-navy">Source:</span> <a href="{{ $url }}" rel="noopener">{{ $name }}</a>, licensed <a href="{{ $licenseUrl }}" rel="license noopener">{{ $license }}</a>.
+        @if($date) Snapshot {{ $date }}. @endif
+        @if($note) {{ $note }} @endif
+    </p>
     @if($citation)<p class="mt-1">Cite as: {{ $citation }}</p>@endif
 </aside>
